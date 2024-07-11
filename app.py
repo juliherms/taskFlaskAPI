a3c1c9a705999b30bdd915debaeb26229fba2c02
Juliherms De Lima Vasconcelos <juliherms.vasconcelos@exmed.com.br>
2024-07-11 08:42:20 -0300

    feat: criacao do endpoint de task

--- app.py
+++ app.py
@@ -1,11 +1,27 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from models.task import Task
 
 app = Flask(__name__)
 
+tasks = []
+task_id_control = 1
 
 @app.route("/")
 def hello_world():
     return "Hello World!"
+
+
+@app.route("/tasks", methods=["POST"])
+def create_task():
+    global task_id_control
+
+    data = request.get_json()
+    new_task = Task(id=task_id_control,title=data.get("title"), description=data.get("description", ""))
+    task_id_control += 1
+    tasks.append(new_task)
+
+    print(tasks)
+    return jsonify({"message": "Nova tarefa criada com sucesso"})
 
 
 @app.route("/about")
