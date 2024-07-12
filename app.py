2bbb4024bc571ba7e1f595a0779cb779ce95d4c8
Juliherms De Lima Vasconcelos <juliherms.vasconcelos@exmed.com.br>
2024-07-12 17:46:26 -0300

    feat: implementacao da consulta e listagem de tasks

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ app = Flask(__name__)
 tasks = []
 task_id_control = 1
 
+
 @app.route("/")
 def hello_world():
     return "Hello World!"
@@ -16,12 +17,38 @@ def create_task():
     global task_id_control
 
     data = request.get_json()
-    new_task = Task(id=task_id_control,title=data.get("title"), description=data.get("description", ""))
+    new_task = Task(
+        id=task_id_control,
+        title=data.get("title"),
+        description=data.get("description", ""),
+    )
     task_id_control += 1
     tasks.append(new_task)
 
     print(tasks)
     return jsonify({"message": "Nova tarefa criada com sucesso"})
+
+
+@app.route("/tasks", methods=["GET"])
+def get_tasks():
+
+    task_list = [task.to_dict() for task in tasks]
+
+    output = {
+        "tasks": task_list,
+        "total_tasks": len(task_list),
+    }
+
+    return jsonify(output)
+
+
+@app.route("/tasks/<int:id>", methods=["GET"])
+def get_task(id):
+    for t in tasks:
+        if t.id == id:
+            return jsonify(t.to_dict())
+
+    return jsonify({"message": "Não foi possível encontrar a atividade"}), 404
 
 
 @app.route("/about")
